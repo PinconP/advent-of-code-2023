97f959466139d7dcd7c2e6d2aa1e3ac50f53efbd
Pierre Pinçon <pierre.pincon@univ-tlse3.fr>
2023-12-03 00:08:33 +0100

    Restructuration for cleaner organization

--- src/Day01/day01.py
+++ src/Day01/day01.py
@@ -1,3 +1,6 @@
+import re
+
+
 def load_puzzle(path):
     # Initialize an empty list to store the lines
     lines = []
@@ -11,6 +14,39 @@ def load_puzzle(path):
 
     # Now lines contains all the lines from the file
     return lines
+
+
+def extract_digits_final(s):
+    # Extensive dictionary mapping spelled out numbers to their digit equivalents
+    number_map = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
+    # Compiling a regular expression pattern for better matching
+    pattern = re.compile(r"(" + "|".join(number_map.keys()) + r"|\d)")
+
+    # List to hold the extracted digits
+    extracted_digits = []
+
+    # Iterating over the matches
+    for match in pattern.finditer(s):
+        word = match.group()
+        # If the word is a spelled out number, convert it to digit
+        if word in number_map:
+            extracted_digits.append(number_map[word])
+        # If the word is a digit, add it directly
+        elif word.isdigit():
+            extracted_digits.append(int(word))
+    return extracted_digits
 
 
 class Puzzle:
@@ -30,22 +66,22 @@ class Puzzle:
         # Iterate over each line
         for line in lines:
             # Extract all digits from the line
-            digits = [int(char) for char in line if char.isdigit()]
-            # Calculate the calibration value for the line
+            digits = extract_digits_final(line)
 
             line_sum = int("".join([str(digits[0]), str(digits[-1])]))
+
             # Add the sum of the current line to the total sum
             total_sum += line_sum
 
-        return total_sum
+        self.solution = total_sum
 
 
 if __name__ == "__main__":
     # Create a new puzzle instance
-    puzzle = Puzzle("puzzle")
+    puzzle = Puzzle("src/Day01/puzzle_test.txt")
 
     # Solve the puzzle
-    solution = puzzle.solve()
+    puzzle.solve()
 
     # Print the solution
-    print(solution)
+    print(puzzle.solution)
