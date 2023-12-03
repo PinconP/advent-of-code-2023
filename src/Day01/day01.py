2ae7b651c7ff8ea812f9372e38703ebb5b264ecc
Pierre Pinçon <pierre.pincon@univ-tlse3.fr>
2023-12-03 16:06:14 +0100

    Update day01.py
    removed function "load_puzzle" to make it a standalone

--- src/Day01/day01.py
+++ src/Day01/day01.py
@@ -1,19 +1,5 @@
 import re
-
-
-def load_puzzle(path):
-    # Initialize an empty list to store the lines
-    lines = []
-
-    # Open the file in read mode
-    with open(path, "r") as file:
-        # Iterate over each line in the file
-        for line in file:
-            # Strip newline characters and append the line to the list
-            lines.append(line.strip())
-
-    # Now lines contains all the lines from the file
-    return lines
+from src.load_puzzle import load_puzzle
 
 
 def extract_digits_final(s):
